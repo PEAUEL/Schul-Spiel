43b0f737907ce8f795af9a6c4c1cc02109dee56b
PEAUEL <reuter.paul1@icloud.com>
2026-06-17 20:45:35 +0200

    Implement gravity and speed mechanics in game loop

--- js/flur.js
+++ js/flur.js
@@ -5,25 +5,23 @@ let playerX = 500;
 let playerY = 100;
 let worldX = 0;
 let levelWidth = 3000;
-let speedX = 0
+let speedX = 0;
+let speedY = 0;
+const gravity = 1;
 gameviewer.width = window.innerWidth;
 gameviewer.height = window.innerHeight;
 
 document.addEventListener('keydown', (Event) => {
     Event.preventDefault()
     if (Event.key === 'd' || Event.key === 'D' || Event.key === 'ArrowRight') {
-        playerX += 10;
+        speedX = 10;
     }
     if (Event.key === 'a' || Event.key === 'A' || Event.key === 'ArrowLeft') {
-        playerX -= 10;
+        speedX = -10;
     }
-    if (Event.key === 'w' || Event.key === 'W' || Event.key === 'ArrowUp') {
-        playerY -= 10;
+    if (Event.key === 'w' || Event.key === 'W' || Event.key === 'ArrowUp' || Event.key === 'Space') {
+        speedY = -10;
     }
-    if (Event.key === 's' || Event.key === 'S' || Event.key === 'ArrowDown') {
-        playerY += 10;
-    }
-})
 
 brush.clearCanvas = function() {
     brush.clearRect(0, 0, gameviewer.width, gameviewer.height);
@@ -35,6 +33,14 @@ brush.drawPlayer = function(x, y, size, color) {
 }
 
 function gameloop() {
+    speedX *= 0.7;
+    speedY -= gravity;
+    playerX += speedX;
+    playerY -= speedY;
+    if (playerY > 600) {
+        playerY = 600;
+        speedY = 0;
+    }
     brush.clearCanvas();
     brush.drawPlayer(playerX, playerY, 100, '#ff0000');
 
